798053228fe70b61bd5df67217368c2196a774cc
VSR-Certifications <adam.drieling@vertexsafetyresources.com>
2026-03-25 14:08:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const DATA_URL = "https://airtableusercontent.com/shrXWlYbXxR05lzGq?format=json";
+  const DATA_URL = "https://airtableusercontent.com/XWlYbXxR05lzGq?format=json";
 
   const searchInput = document.getElementById("search");
   const cardContainer = document.getElementById("cardContainer");
